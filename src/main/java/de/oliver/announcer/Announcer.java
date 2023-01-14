4eca43e30f77146bd52d22847762dc5ae73a7212
OliverSchlueter <oliverhd.1303@gmail.com>
2023-01-14 15:34:20 +0100

    Init

---
+++ src/main/java/de/oliver/announcer/Announcer.java
@@ -0,0 +1,17 @@
+package de.oliver.announcer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Announcer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
